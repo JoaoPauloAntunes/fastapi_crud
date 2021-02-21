fcfc8cf213e4db8a11631b848b8b517ada62c771
joaopauloantunes <jpantunesdesouza@gmail.com>
2021-02-21 19:57:26 -0300

    add route

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from .routes.views_routes import views_router
 from .routes.user_routes import user_router
 from .routes.test_routes import test_router
+from .routes.student_routes import student_router
 
 
 app = FastAPI()
@@ -18,3 +19,4 @@ def root():
 app.include_router(views_router, prefix="/views", tags=["views"])
 app.include_router(user_router, prefix="/users", tags=["users"])
 app.include_router(test_router, prefix="/tests", tags=["tests"])
+app.include_router(student_router, prefix="/students", tags=["students"])
---
+++ src/routes/student_routes.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, HTTPException, status, APIRouter
+from typing import Dict, List, Optional, Union
+import json
+
+
+students = json.load(open("data/students.json", "r"))
+student_router = APIRouter()
+
+
+@student_router.get("/")
+def get_all_students() -> List[Dict[str, Union[float, int, str]]]:
+    if response := students:
+        return response
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Não existem estudantes cadastrados.",
+    )
+    
+
+@student_router.get("/{student_id}/")
+def get_student(
+    student_id: int
+) -> Dict[str, Union[float, int, str]]:
+    if response := list(
+        filter(lambda i: i.get("id") == student_id, students)
+    ):
+        return response[0]
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Estudante de 'id={student_id}' não encontrado.",
+    )
+
+
+@student_router.get("/{student_id}/")
+def delete_student(
+    student_id: int
+) -> Dict[str, bool]:
+    if response := list(
+        filter(lambda i: i.get("id") == student_id, students)
+    ):
+        del students[students.index(response)]
+        return {"success": True}
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Estudante de 'id={student_id}' não encontrado.",
+    )
